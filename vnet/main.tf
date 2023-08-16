75db701dca68771947963967d325e6dc092643a2
Vyankatesh5 <vyankatesh.muley5@gmail.com>
2023-08-16 16:12:42 +0530

    Update main.tf

--- vnet/main.tf
+++ vnet/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "example" {
-  name                = var.vnet_name"
+  name                = var.vnet_name
   location            = var.vnet_location
   resource_group_name = var.vnet_resource_group_name
   address_space       = var.vnet_address_space
