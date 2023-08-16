4b4cf6f5c119686582b61b15053d7af2ff6cd0a4
Vyankatesh5 <vyankatesh.muley5@gmail.com>
2023-08-16 16:12:17 +0530

    Update provider.tf

--- vnet/provider.tf
+++ vnet/provider.tf
@@ -1,32 +1,42 @@
-terraform {
+ 
 
 required_providers {
 
+ 
+
 azurerm = {
+
+ 
 
 source ="hashicorp/azurerm"
 
+ 
+
 version= "~>2.15.0"
-
-
-
-
+}
+}
 }
 
-}
-
-}
+ 
 
 provider "azurerm" {
 
-subscription_id = var.subscription_id
+ 
 
-  client_id       = var.client_id
+subscription_id = "447d3258-c6b9-45ac-8bcf-e343def42272"
 
-  client_secret   = var.client_secret
+ 
 
-  tenant_id       = var.tenant_id
+  client_id       = "cfcb55f7-d52d-48e4-b501-0adc37700358"
+
+ 
+
+  client_secret   = "J~J8Q~Fk5RKQox2KiYAKyKQCS1C7DA0XG1Mc~cs9"
+
+ 
+
+  tenant_id       = "2f2e0da1-0d79-4d2b-b60c-9e541463f9f9"
+
+ 
 
 features {}
-
-}
