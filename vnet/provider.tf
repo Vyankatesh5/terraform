12ebf81aa308aad9edf5fe966844a1ad5a13eb4e
Vyankatesh5 <vyankatesh.muley5@gmail.com>
2023-08-16 16:49:00 +0530

    Update provider.tf

--- vnet/provider.tf
+++ vnet/provider.tf
@@ -23,19 +23,19 @@ provider "azurerm" {
 
  
 
-subscription_id = "447d3258-c6b9-45ac-8bcf-e343def42272"
+subscription_id = ""
 
  
 
-  client_id       = "cfcb55f7-d52d-48e4-b501-0adc37700358"
+  client_id       = ""
 
  
 
-  client_secret   = "J~J8Q~Fk5RKQox2KiYAKyKQCS1C7DA0XG1Mc~cs9"
+  client_secret   = ""
 
  
 
-  tenant_id       = "2f2e0da1-0d79-4d2b-b60c-9e541463f9f9"
+  tenant_id       = ""
 
  
 
