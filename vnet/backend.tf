c85eb68acf49ffae9e2cd14164bbc44844d7d234
Vyankatesh5 <vyankatesh.muley5@gmail.com>
2023-08-18 09:39:49 +0530

    Update backend.tf

--- vnet/backend.tf
+++ vnet/backend.tf
@@ -1,8 +1,10 @@
-terraform {
+erraform {
   backend "azurerm" {
     resource_group_name  = "RG_India"
     storage_account_name = "azmonitoring"
     container_name       = "volpay"
-    key                  = "test/terraform.tfstate"
+    key                  = "terraform.tfstate"
+    access_key         = "rZX6WEeN8o9MyBUpfvTr524y7iV6/YA+DFFXcT2MQWgmMf+79jd+U4THV44b5glT/+ML04YIMlqR+ASt0ozSbA=="
+
   }
 }
