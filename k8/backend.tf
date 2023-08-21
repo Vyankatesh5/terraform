073e68cf9b20e466d042386a3533647aa7a750c9
Vyankatesh5 <vyankatesh.muley5@gmail.com>
2023-08-21 10:18:20 +0530

    Update backend.tf

--- k8/backend.tf
+++ k8/backend.tf
@@ -5,3 +5,8 @@ terraform {
     container_name       = "volpay"
     key                  = "kcluster/terraform.tfstate"
     access_key         = "rZX6WEeN8o9MyBUpfvTr524y7iV6/YA+DFFXcT2MQWgmMf+79jd+U4THV44b5glT/+ML04YIMlqR+ASt0ozSbA=="
+
+ 
+
+  }
+}
