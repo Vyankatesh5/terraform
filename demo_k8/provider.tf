f9b767296b205ac3c4add6d2cfd676698c595245
Vyankatesh5 <vyankatesh.muley5@gmail.com>
2023-09-11 14:04:44 +0530

    Update provider.tf

--- demo_k8/provider.tf
+++ demo_k8/provider.tf
@@ -8,9 +8,9 @@ version= "~>2.15.0"
 }
 }
 provider "azurerm" {
-subscription_id = var.subscription_id 
-  client_id       = var.client_id 
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+//subscription_id = var.subscription_id 
+//  client_id       = var.client_id 
+//  client_secret   = var.client_secret
+//  tenant_id       = var.tenant_id
 features {}
 }
