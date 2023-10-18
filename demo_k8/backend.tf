4830246dbbd1aefb8f937ccfdc1b27877ef64314
Vyankatesh5 <vyankatesh.muley5@gmail.com>
2023-10-18 13:32:48 +0530

    Update backend.tf

--- demo_k8/backend.tf
+++ demo_k8/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "Cloud_Asset_Platform-RG-7KBNA"
-    storage_account_name = "azmonitoring"
+    storage_account_name = "kaiburr"
     container_name       = "demo-kaiburr"
     key                  = "kcluster/terraform.tfstate"
-    access_key         = "rZX6WEeN8o9MyBUpfvTr524y7iV6/YA+DFFXcT2MQWgmMf+79jd+U4THV44b5glT/+ML04YIMlqR+ASt0ozSbA=="
+    access_key         = "iMLPpa0HNzThJyih67nMVMb7DaJ4a/GD8VTETQpyQoc3GRjMwOlJSWwqEWUDT5YrhXzPSzhweqX5+AStT+jPkg=="
 
   }
 }
